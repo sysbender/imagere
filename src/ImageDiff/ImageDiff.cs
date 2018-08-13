c5c184181ad28f8a6b4af07a5a76ad00af101945
sysbender <sysbender@gmail.com>
2018-08-12 22:28:00 -0400

    basic compare -- not completed

---
+++ src/ImageDiff/ImageDiff.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImageMagick;
+using System.IO;
+
+namespace Imagere
+{
+    class ImageDiff
+    {
+
+        // folders
+        public String CurrentImageFolder { get; set; }
+        public String BaselineImageFolder { get; set; }
+        public String DiffImageFolder { get; set; }
+
+
+        // compare options
+
+
+
+        // constructor
+
+        public ImageDiff(String currentImageFolder, String baselineImageFolder, String DiffImageFolder)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+
+        public void CompareFile(String imageFileName)
+        {
+            var currentImage = new MagickImage(PathCombine(CurrentImageFolder, imageFileName));
+            var baselineImage = new MagickImage(PathCombine(BaselineImageFolder, imageFileName));
+            try
+            {
+
+                using (var diff = new MagickImage())
+                {
+                    currentImage.Compare(baselineImage, ErrorMetric.PeakAbsolute, diff);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+
+
+        }
+
+
+        private String PathCombine(String dir, String fileName)
+        {
+            if (Path.GetFileName(fileName) != fileName)
+            {
+                throw new Exception("'fileName' is invalid!");
+            }
+
+
+            return Path.Combine(dir, Path.GetFileName(fileName));
+        }
+
+
+    }
+}
--- src/UpdateBaseline/Program.cs
+++ src/UpdateBaseline/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UpdateBaseline
+namespace Imagere
 {
-    class Program
+    class UpdateBaseline
     {
         static void Main(string[] args)
         {
